b780422cda6fea8b0687fb0df925eba1693f5320
JayanUpasara-prog <jayan.upasara@gmail.com>
2023-12-09 17:36:44 +0530

    header navigation including

--- resources/views/user/SBU_LogsTimber.blade.php
+++ resources/views/user/SBU_LogsTimber.blade.php
@@ -17,52 +17,7 @@
 </head>
 <body>
 
-<!-- <div class="p-2 bg-light text-black text-center">
-  <img src="Srilanka.jpg" class="float-start img-fluid" alt="Srilanka" width="100" height="100">
-  <h1 class="text-success text-start">Forest Department,</h1>
-  <p class="text-dark text-start">Melsiripura</p> 
-</div> -->
-
-<nav class="navbar bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="homepage">
-      <img src="assets/images/dashboardImg/Srilanka.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      <span class="text-success">Forest Department,</span>
-      Melsiripura
-    </a>
-    <form class="d-flex mx-auto text-right" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    <div class="btn-group">
-      <button type="button" class="btn btn-light dropdown-toggle btn-outline-success" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="assets/images/dashboardImg/face.jpg" alt="Profile Photo" class="rounded-circle" width="20" height="20">
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end">
-          <li><a class="dropdown-item" href="#">My Profile</a></li>
-          <li><a class="dropdown-item" href="#">Log Out</a></li>
-      </ul>
-  </div>
-</nav>
-
-<!-- <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav> -->
+@include('userHeadNav')
 </br>
 
 <div class="container-fluid">
@@ -155,46 +110,7 @@
 </div>
 
 
-<!-- <div class="container mt-5">
-  <div class="row">
-    <div class="col-sm-4">
-      <h2>About Me</h2>
-      <h5>Photo of me:</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
-      <h3 class="mt-4">Some Links</h3>
-      <p>Lorem ipsum dolor sit ame.</p>
-      <ul class="nav nav-pills flex-column">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#">Disabled</a>
-        </li>
-      </ul>
-      <hr class="d-sm-none">
-    </div>
-    <div class="col-sm-8">
-      <h2>TITLE HEADING</h2>
-      <h5>Title description, Dec 7, 2020</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
 
-      <h2 class="mt-5">TITLE HEADING</h2>
-      <h5>Title description, Sep 2, 2020</h5>
-      <div class="fakeimg">Fake Image</div>
-      <p>Some text..</p>
-      <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
-    </div>
-  </div>
-</div> -->
 
 <!-- ======= Footer ======= -->
 @include('footer')
--- resources/views/user/UserDashboard.blade.php
+++ resources/views/user/UserDashboard.blade.php
@@ -17,52 +17,8 @@
 </head>
 <body>
 
-<!-- <div class="p-2 bg-light text-black text-center">
-  <img src="Srilanka.jpg" class="float-start img-fluid" alt="Srilanka" width="100" height="100">
-  <h1 class="text-success text-start">Forest Department,</h1>
-  <p class="text-dark text-start">Melsiripura</p> 
-</div> -->
+@include('userHeadNav')
 
-<nav class="navbar bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="homepage">
-      <img src="assets/images/dashboardImg/Srilanka.jpg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
-      <span class="text-success">Forest Department,</span>
-      Melsiripura
-    </a>
-    <form class="d-flex mx-auto text-right" role="search">
-      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success" type="submit">Search</button>
-    </form>
-    <div class="btn-group">
-      <button type="button" class="btn btn-light dropdown-toggle btn-outline-success" data-bs-toggle="dropdown" aria-expanded="false">
-          <img src="assets/images/dashboardImg/face.jpg" alt="Profile Photo" class="rounded-circle" width="20" height="20">
-      </button>
-      <ul class="dropdown-menu dropdown-menu-end">
-          <li><a class="dropdown-item" href="#">My Profile</a></li>
-          <li><a class="dropdown-item" href="#">Log Out</a></li>
-      </ul>
-  </div>
-</nav>
-
-<!-- <nav class="navbar navbar-expand-sm bg-primary navbar-dark">
-  <div class="container-fluid">
-    <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link active" href="#">Active</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-  </div>
-</nav> -->
 </br>
 
 <div class="container-fluid">
