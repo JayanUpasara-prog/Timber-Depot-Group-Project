995a65cf5f1cca453aee57a89c87211aa0c553a5
JayanUpasara-prog <jayan.upasara@gmail.com>
2023-12-09 14:40:48 +0530

    footer including

--- resources/views/user/StockBookUpdate.blade.php
+++ resources/views/user/StockBookUpdate.blade.php
@@ -155,72 +155,8 @@
 </div> -->
 
 <!-- ======= Footer ======= -->
-<footer class="footer mt-2">
-  <div class="container">
-    <div class="row">
-
-      <div class="col-md-12 col-lg-4">
-        <div class="footer-logo">
-
-          <a class="navbar-brand" href="#">Forest Department,<br>Melsiripura.</a>
-          <p> HF4C+JG4, Ibbagamuwa, <br> Melsiripura.<br>
-              info@example.com<br>
-              0372259591</p>
-        </div>
-      </div>
-
-
-      <div class="col-md-12 col-lg-4">
-        <div class="list-menu">
-
-          <h4>About Us</h4>
-
-       <p>In our island's forests, we enjoy nature's gifts: fresh air, pure water,diverse life, and vital ecosystem services. The Forest Department diligently safeguards these invaluable landscapes. It also serves the public at headquarters and in the field. Together, we nurture our natural treasures for all to cherish.</p>
-        </div>
-      
-      </div>
-
-
-
-      <div class="col-sm-6 col-md-3 col-lg-2">
-        <div class="list-menu">
-
-          <h4>Quick Links</h4>
-      
-          <ul class="list-unstyled">
-            <li><a href="homepage">Home</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Sign in</a></li>
-            <li><a href="#">My Account</a></li>
-          </ul>
-
-        </div>
-      </div>
-
-    
-      
-      <div class="col-sm-6 col-md-3 col-lg-2">
-        <div class="list-menu">
-
-          <h4>Follow Us</h4>
-      
-          <ul class="list-unstyled">
-            <li><a href="" class="twitter"><i class="bi bi-twitter"></i></a></li>
-            <li> <a href="#" class="facebook"><i class="bi bi-facebook"></i></a></li>
-            <li><a href="#" class="instagram"><i class="bi bi-instagram"></i></a></li>
-            <li><a href="#" class="gmail"><i class="bi bi-google"></i></a></li>
-          </ul>
-
-        </div>
-      </div>
-
-
-    </div>
-  </div>
-
-  
-
-</footer><!-- End  Footer -->
+@include('footer')
+<!-- End  Footer -->
 
 </body>
 </html>
